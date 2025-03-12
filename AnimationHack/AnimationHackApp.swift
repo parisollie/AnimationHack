32b8502dc366ed9250afd47234279d53877b230d
parisollie <pjff18@gmail.com>
2025-03-12 10:19:21 -0600

    Initial Commit

---
+++ AnimationHack/AnimationHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationHackApp.swift
+//  AnimationHack
+//
+//  Created by Paul F on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
